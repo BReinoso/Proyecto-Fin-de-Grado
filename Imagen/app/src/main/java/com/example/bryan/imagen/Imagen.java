b2079d7d99821abc326e130730abceb00cfb6729
garfio1 <bryansons@hotmail.com>
2015-05-06 21:20:11 +0200

    Client with Post Request
    New version of client with a try to a post request to flask serverr. Now
    it have some errors.

--- Imagen/app/src/main/java/com/example/bryan/imagen/Imagen.java
+++ Imagen/app/src/main/java/com/example/bryan/imagen/Imagen.java
@@ -6,33 +6,66 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.OpenableColumns;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class Imagen extends ActionBarActivity {
     private int SELECT_IMAGE = 237;
     private int TAKE_PICTURE = 829;
+    private int n_touchs=0;
     private EditText lblPhoto;
-    private Button btnPhoto;
     private ImageView imgPhoto;
+    private RelativeLayout world;
+    private HttpClient client;
+    private HttpPost post=new HttpPost("192.168.1.138");
+    private MultipartEntityBuilder builder=MultipartEntityBuilder.create();
+    private HttpResponse response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagen);
         lblPhoto=(EditText)findViewById(R.id.lblPhoto);
-        btnPhoto=(Button)findViewById(R.id.btnPhoto);
         imgPhoto=(ImageView)findViewById(R.id.imgPhoto);
-        btnPhoto.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                dialogPhoto();
+        world=(RelativeLayout)findViewById(R.id.world);
+        client=new DefaultHttpClient();
+        builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+        world.setOnTouchListener(new RelativeLayout.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (n_touchs == 0) {
+                    dialogPhoto();
+                    n_touchs++;
+                }
+                return true;
             }
         });
     }
@@ -60,7 +93,9 @@ public class Imagen extends ActionBarActivity {
             });
             AlertDialog alert = builder.create();
             alert.show();
-        } catch(Exception e){}
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -92,25 +127,42 @@ public class Imagen extends ActionBarActivity {
             if (requestCode == SELECT_IMAGE) {
                 if (resultCode == Activity.RESULT_OK) {
                     Uri selectedImage = data.getData();
-                    lblPhoto.setText(getPath(selectedImage));
                     imgPhoto.setImageURI(selectedImage);
+                    File file_image=new File(selectedImage.toString());
+                    FileBody file_send=new FileBody(file_image);
+                    builder.addPart("file",file_send);
+                    HttpEntity entity=builder.build();
+                    post.setEntity(entity);
+                    response=client.execute(post);
+                    lblPhoto.setText(getContent(response));
                 }
             }
             if(requestCode == TAKE_PICTURE) {
                 if (resultCode == Activity.RESULT_OK) {
                     Uri selectedImage = data.getData();
-                    lblPhoto.setText(getPath(selectedImage));
+                    lblPhoto.setText("ok");
                     imgPhoto.setImageURI(selectedImage);
+                    File file_image=new File(selectedImage.toString());
+                    FileBody file_send=new FileBody(file_image);
+                    builder.addPart("file",file_send);
+                    post.setEntity(builder.build());
+                    response=client.execute(post);
+                    lblPhoto.setText(getContent(response));
                 }
             }
-        } catch(Exception e){}
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
-    private String getPath(Uri uri) {
-        /*String[] projection = { android.provider.MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        int column_index = cursor.getColumnIndexOrThrow(android.provider.MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);*/
-        return new String("Ok");
+    public static String getContent(HttpResponse response) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        String body = "";
+        String content = "";
+
+        while ((body = rd.readLine()) != null)
+        {
+            content += body + "\n";
+        }
+        return content.trim();
     }
 }
