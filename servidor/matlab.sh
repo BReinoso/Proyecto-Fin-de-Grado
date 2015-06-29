ab01e0ecc4339fc67bd633a6dd6aef90bf655da2
garfio1 <bryansons@hotmail.com>
2015-06-29 23:53:08 +0200

    Server changes added
    A little performance of the servet

---
+++ servidor/matlab.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/MATLAB/R2014b/bin/matlab -nojvm -r "run('/home/bryan/neuraltalk/matlab_features_reference/extract_features.m');quit;"
+
--- servidor/serv.py
+++ servidor/serv.py
@@ -6,18 +6,22 @@ import urllib
 #---------------------
 import os
 #import matlab_wrapper
-import goslate #Libreria de traduccion
+#import goslate #Libreria de traduccion
 from flask import render_template
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, Response
 from werkzeug import secure_filename
+#Ruta hacia el directorio de NeuralTalk
+NEURAL_FOLDER='/home/bryan/neuraltalk/'
+#Ruta hacia el directorio del servidor
+SERV_FOLDER = '/home/bryan/Proyecto-Fin-de-Grado/servidor/'
 #Ruta hacia el directorio uploads de donde tengas el servidor 
-UPLOAD_FOLDER = '/home/bryan/Proyecto-Fin-de-Grado/servidor/uploads/'
+UPLOAD_FOLDER = SERV_FOLDER+'uploads/'
 #Rute hacia los templates
-TEMPLATE_FOLDER='/home/bryan/Proyecto-Fin-de-Grado/servidor/templates/'
+TEMPLATE_FOLDER=SERV_FOLDER+'templates/'
 #Ruta para modificar el archivo task y reconocer la imagen
 TASK_TXT=UPLOAD_FOLDER+'tasks.txt'
 #De momento solo admite  imagenes .jpg
-ALLOWED_EXTENSIONS = set(['jpg','JPG'])
+ALLOWED_EXTENSIONS = set(['jpg','JPG','jpeg','JPEG'])
  
 app = Flask(__name__)
 #Annadimos el directorio donde se suben los archivos a la configuracion del servidor
@@ -38,57 +42,45 @@ def index():
 	    f.write(file.filename+'\n')
 	    f.close()
 	    #Ejecutamos el script para extraer las caractersticas de la imagen
-	    os.system("sudo -u bryan /home/bryan/Proyecto-Fin-de-Grado/servidor/matlab.sh")
+	    os.system("sudo -u bryan "+SERV_FOLDER+"/matlab.sh")
 	    #Ejecutamos el NeuralTalk para predecir el caption
-	    os.system("python /home/bryan/neuraltalk/predict_on_images.py /home/bryan/neuraltalk/cv/model_checkpoint_coco_visionlab43.stanford.edu_lstm_11.14.p -r /home/bryan/Proyecto-Fin-de-Grado/servidor/uploads/")
+	    os.system("python "+NEURAL_FOLDER+"predict_on_images.py "+NEURAL_FOLDER+"cv/model_checkpoint_coco_visionlab43.stanford.edu_lstm_11.14.p -r "+UPLOAD_FOLDER)
+	    os.system("rm "+UPLOAD_FOLDER+file.filename)
 	    #Movemos el result.html a la carpeta templates
-	    os.system("cp "+UPLOAD_FOLDER+"result.html "+TEMPLATE_FOLDER)
+	    #os.system("cp "+UPLOAD_FOLDER+"result.html "+TEMPLATE_FOLDER)
 	    #Abrimos el fichero y extraemos solamente el caption, ose laa prediccion
-	    html_file=open(TEMPLATE_FOLDER+"result.html","r")
+	    html_file=open(UPLOAD_FOLDER+"result.html","r")
 	    strs=html_file.read()
        	    strs=strs.split(')')
 	    strs=strs[1]
 	    strs=strs.split('<')
-	    strs=strs[0]
+	    strs=strs[0] 
 	    strs=traducir(strs)
 	    #Escribiendo en el html el resultado final
 	    html_file.close()
-	    html_file=open(TEMPLATE_FOLDER+"result.html","w")
-	    html_file.write("<html><body>"+strs+"<br></body></html>")
-	    html_file.close()
-            return render_template('result.html')#Hacemos que el cliente vea la prediccion
-	#Html que visualizara el cliente
-    return """
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <p>%s</p>
-    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+	    #html_file=open(TEMPLATE_FOLDER+"result.html","w")
+	    #html_file.write("<html><body>"+strs+"<br></body></html>")
+	    #html_file.close()
+	    print request.headers
+        return strs
+    #Html que visualizara el cliente cuando se hace peticion get desde navegador
+    return render_template("index.html")
 
 def traducir(string='Hola'):
 	data = {'sl':'en','tl':'es','text':string} 
 	querystring = urllib.urlencode(data)
 	request = urllib2.Request('http://www.translate.google.com' + '?' + querystring )
-
 	request.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11')
 	opener = urllib2.build_opener()
 	feeddata = opener.open(request).read()
-	#print feeddata
 	soup = BeautifulSoup(feeddata)
 	strs=soup.find('span', id="result_box")
 	strs=str(strs)
-	#print "RECIBIDO EN TRADUCIR"+strs
 	strs=strs.split('">')
 	strs=strs[3]
-	#print "PRIMER SPLIT"+strs
 	strs=strs.split('<')
 	strs=strs[0]
-	#print "SEGUNDO SPLIT"+strs
 	return strs
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(80), debug=True)
+    app.run(host='0.0.0.0', port=int(80), debug=True,threaded=True)
